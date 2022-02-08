3ffd6ca123ed967cca52e1d953d5050e1cc528b8
Jiajie Chen <c@jia.je>
2022-02-08 08:38:25 +0800

    Add more tests, it seems that sqrt lowering is missing

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 
 #include "riscv_vector.h"
+#include <math.h>
 #include <stdlib.h>
 
 extern "C" {
@@ -13,8 +14,8 @@ void test_cond(uint64_t n, double *__restrict x) {
   }
 }
 
-void test_axpy(uint64_t n, double a, const double *__restrict x,
-               double *__restrict y) {
+void test_axpy_inner(uint64_t n, double a, const double *__restrict x,
+                     double *__restrict y) {
   uint64_t vlmax = vsetvlmax_e64m1();
   uint64_t i;
   vfloat64m1_t x_data = vle64_v_f64m1(x, vlmax);
@@ -22,5 +23,19 @@ void test_axpy(uint64_t n, double a, const double *__restrict x,
   y_data = vfmacc(y_data, a, x_data, vlmax);
 
   vse64_v_f64m1(&y[i], y_data, vlmax);
+}
+
+void test_l1dist(uint64_t n, double *__restrict res, const double *__restrict x,
+               const double *__restrict y) {
+  for (int i = 0; i < n; i++) {
+    res[i] = abs(x[i]) + abs(y[i]);
+  }
+}
+
+void test_l2dist(uint64_t n, double *__restrict res, const double *__restrict x,
+               const double *__restrict y) {
+  for (int i = 0; i < n; i++) {
+    res[i] = sqrt(x[i] * x[i] + y[i] * y[i]);
+  }
 }
 }
